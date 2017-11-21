1fb8445da1146920e3bcc0927beda452e95ba580
gaoqionglou <438279375@qq.com>
2017-11-21 17:30:01 +0800

    对话框基类

---
+++ app/src/main/java/com/wke/gql/base/BaseDialogFragment.java
@@ -0,0 +1,62 @@
+package com.wke.gql.base;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import java.lang.reflect.Field;
+
+public abstract class BaseDialogFragment extends DialogFragment {
+
+    protected abstract Dialog dialog();
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return dialog();
+    }
+
+    @Override
+    public void show(FragmentManager manager, String tag) {
+        // 如果使用api本身自带的 show() 方法，调用的是 commit() 方法添加 DialogFragment，这样的话会导致
+        // 当 Activity 调用 onSaveInstanceState() 方法后加载弹出框时报 IllegalStateException 异常，
+        // 所以需要通过反射重写 show() 方法，使用 commitAllowingStateLoss() 方法添加 DialogFragment.
+        try {
+            Class<?> classDialogFragment = getClass().getSuperclass();
+            while (!classDialogFragment.equals(DialogFragment.class)) {
+                classDialogFragment = classDialogFragment.getSuperclass();
+            }
+            Field mDismissed = classDialogFragment.getDeclaredField("mDismissed");
+            mDismissed.setAccessible(true);
+            mDismissed.set(this, false);
+            Field mShownByMe = classDialogFragment.getDeclaredField("mShownByMe");
+            mShownByMe.setAccessible(true);
+            mShownByMe.set(this, false);
+            FragmentTransaction ft = manager.beginTransaction();
+            ft.add(this, tag);
+            ft.commitAllowingStateLoss();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dismissAllowingStateLoss() {
+        if (getDialog() != null && getDialog().isShowing()) super.dismissAllowingStateLoss();
+    }
+
+/*    protected Dialog getBaseDialog() {
+        Dialog dialog = new Dialog(getActivity(), R.style.CSMBPDialogFragment);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = dialog.getWindow();
+            if (window != null) window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        return dialog;
+    }*/
+}
