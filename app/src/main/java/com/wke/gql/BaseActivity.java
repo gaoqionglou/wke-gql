50ce2164c50e166159f535dee1e548e4566de1f5
gaoqionglou <438279375@qq.com>
2017-11-27 14:44:29 +0800

    在BaseActivity的onDestory,onStop调用networkutil的取消网络请求的方法

--- app/src/main/java/com/wke/gql/BaseActivity.java
+++ app/src/main/java/com/wke/gql/BaseActivity.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.wke.gql.base.BaseApplication;
-import com.wke.gql.net.NetWorkUtil;
 import com.wke.gql.net.RxNetWorkUtil;
 
 public class BaseActivity extends AppCompatActivity {
     public RxNetWorkUtil rxNetWorkUtil;
-    public NetWorkUtil netWorkUtil;
+
+    //    public NetWorkUtil netWorkUtil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rxNetWorkUtil = ((BaseApplication) getApplication()).injectRxNetWorkUtilComponent().rxNetWorkUtil();
-        netWorkUtil = ((BaseApplication) getApplication()).injectNetWorkUtilComponent().netWorkUtil();
+//        netWorkUtil = ((BaseApplication) getApplication()).injectNetWorkUtilComponent().netWorkUtil();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (rxNetWorkUtil != null) rxNetWorkUtil.unDisposable();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (rxNetWorkUtil != null) rxNetWorkUtil.unDisposable();
     }
 }
