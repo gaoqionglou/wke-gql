05689e9774e6480e5ca535a185c73592ffa3b019
gaoqionglou <438279375@qq.com>
2017-11-27 22:56:34 +0800

    CoordinatorLayout 示例demo

---
+++ app/src/main/java/com/wke/gql/layoutdemo/activity/ui/GridAdapter.java
@@ -0,0 +1,93 @@
+package com.wke.gql.layoutdemo.activity.ui;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.squareup.picasso.Picasso;
+import com.wke.gql.R;
+import com.wke.gql.layoutdemo.activity.net.Meizi;
+
+import java.util.List;
+
+/**
+ * Created by 赵晨璞 on 2016/6/19.
+ * RecyclerView适配器
+ */
+
+public class GridAdapter extends RecyclerView.Adapter<GridAdapter.MyViewHolder> implements View.OnClickListener, View.OnLongClickListener {
+
+    private Context mContext;
+    private List<Meizi> datas;
+    private OnRecyclerViewItemClickListener mOnItemClickListener = null;
+
+    public GridAdapter(Context context, List<Meizi> datas) {
+        mContext = context;
+        this.datas = datas;
+    }
+
+    public void setOnItemClickListener(OnRecyclerViewItemClickListener listener) {
+        mOnItemClickListener = listener;
+    }
+
+    @Override
+    public GridAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext
+        ).inflate(R.layout.grid_meizi_item, parent,
+                false);
+        MyViewHolder holder = new MyViewHolder(view);
+
+        view.setOnClickListener(this);
+        view.setOnLongClickListener(this);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(GridAdapter.MyViewHolder holder, int position) {
+        Picasso.with(mContext).load(datas.get(position).getUrl()).into(holder.iv);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas.size();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mOnItemClickListener != null) {
+
+            mOnItemClickListener.onItemClick(v);
+        }
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if (mOnItemClickListener != null) {
+            mOnItemClickListener.onItemLongClick(v);
+        }
+        return false;
+    }
+
+
+    public interface OnRecyclerViewItemClickListener {
+        void onItemClick(View view);
+
+        void onItemLongClick(View view);
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        private ImageButton iv;
+
+        public MyViewHolder(View view) {
+            super(view);
+            iv = (ImageButton) view.findViewById(R.id.iv);
+        }
+    }
+
+}
