f67ad0adba952a51053704661dade5b4306a8a0f
gaoqionglou <438279375@qq.com>
2017-11-27 15:57:55 +0800

    update

--- app/src/main/java/com/wke/gql/BaseActivity.java
+++ app/src/main/java/com/wke/gql/BaseActivity.java
@@ -4,28 +4,31 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.wke.gql.base.BaseApplication;
+import com.wke.gql.net.NetWorkUtil;
 import com.wke.gql.net.RxNetWorkUtil;
 
 public class BaseActivity extends AppCompatActivity {
     public RxNetWorkUtil rxNetWorkUtil;
 
-    //    public NetWorkUtil netWorkUtil;
+    public NetWorkUtil netWorkUtil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rxNetWorkUtil = ((BaseApplication) getApplication()).injectRxNetWorkUtilComponent().rxNetWorkUtil();
-//        netWorkUtil = ((BaseApplication) getApplication()).injectNetWorkUtilComponent().netWorkUtil();
+        netWorkUtil = ((BaseApplication) getApplication()).injectNetWorkUtilComponent().netWorkUtil();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (netWorkUtil != null) netWorkUtil.cancel();
         if (rxNetWorkUtil != null) rxNetWorkUtil.unDisposable();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        if (netWorkUtil != null) netWorkUtil.cancel();
         if (rxNetWorkUtil != null) rxNetWorkUtil.unDisposable();
     }
 }
