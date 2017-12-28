4858ffc807526901dfc7d54b7b9d3b7cd8abf937
gaoqionglou <438279375@qq.com>
2017-12-28 10:01:35 +0800

    优化

--- app/src/main/java/com/wke/gql/view/FlowLayout.java
+++ app/src/main/java/com/wke/gql/view/FlowLayout.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,11 +17,14 @@ import java.util.TreeMap;
 
 /**
  * 流式布局 每个孩子需要高度一致，宽度可以不一样
+ * 具有的局限性：1.测量的时候并没有对测量模式做判断,仅仅当成AT_MOST来做
+ * 2.没对容器和孩子margin,padding等做处理
+ * more...
  * Created by gql on 17-12-14.
  */
 public class FlowLayout extends ViewGroup {
     private static final String TAG = "FlowHorizontalLayout";
-    private int maxWidth;
+    private int maxWidth = 0;
     private TreeMap<Integer, Row> rowMap = new TreeMap<>();
     private int horizontal_space, vertical_space;
 
@@ -63,19 +65,19 @@ public class FlowLayout extends ViewGroup {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int width = MeasureSpec.getSize(widthMeasureSpec);//获取容器推荐宽度
         maxWidth = width - getPaddingLeft() - getPaddingRight();
-        Log.i(TAG, "maxWidth: " + maxWidth);
+//        Log.i(TAG, "maxWidth: "+maxWidth);
         rowMap.clear();
         int sum = 0;
-        int rowIndex = 1;
+        int rowIndex = 1;  //第一行开始
         //按顺序遍历孩子
         for (int i = 0; i < getChildCount(); i++) {
             Row row = null;
-            //第一行开始
             View child = getChildAt(i);
+            //先测量孩子
             measureChild(child, widthMeasureSpec, heightMeasureSpec);//测量孩子
-
-            if (maxWidth - sum >= child.getMeasuredWidth() + horizontal_space) {
-                Log.i(TAG, "sum=" + sum + "," + child.getTag().toString() + "属于" + rowIndex + ",长度=" + (child.getMeasuredWidth() + horizontal_space));
+            if (maxWidth - sum > child.getMeasuredWidth() + horizontal_space) {
+                //计算剩余控件是否足够容纳这个孩子
+//                Log.i(TAG,"sum="+sum+","+child.getTag().toString()+"属于"+rowIndex+",长度="+(child.getMeasuredWidth()+horizontal_space));
                 if (rowMap.get(rowIndex) != null) {
                     row = rowMap.get(rowIndex);
                 } else {
@@ -86,9 +88,10 @@ public class FlowLayout extends ViewGroup {
                 rowMap.put(rowIndex, row);
                 sum = sum + child.getMeasuredWidth() + horizontal_space;
             } else {
+                //不够容纳则换行,sum则从换行的第一个孩子算起
                 sum = child.getMeasuredWidth() + horizontal_space;
                 rowIndex++;
-                Log.i(TAG, child.getTag().toString() + "属于" + rowIndex + ",长度=" + (child.getMeasuredWidth() + horizontal_space));
+//                Log.i(TAG,child.getTag().toString()+"属于"+rowIndex+",长度="+(child.getMeasuredWidth()+horizontal_space));
                 row = new Row();
                 row.views.clear();
                 row.views.add(child);
@@ -96,8 +99,8 @@ public class FlowLayout extends ViewGroup {
             }
         }
         //打印出来看下分行结果
-        Log.i(TAG, "onMeasure: " + rowMap.toString());
-        int height = getTotalHeight(rowMap) + vertical_space;/*这是最后一行孩子距离父容器底部之间的间距*/
+//        Log.i(TAG, "onMeasure: " + rowMap.toString());
+        int height = getTotalHeight(rowMap);/*这是最后一行孩子距离父容器底部之间的间距*/
         //最终确定了容器大小
         setMeasuredDimension(width, height);
     }
@@ -105,14 +108,13 @@ public class FlowLayout extends ViewGroup {
     //根据分行结果放置孩子
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        Log.i(TAG, l + "|" + t + "|" + r + "|" + b);
         int left = getPaddingLeft();
         int top = getPaddingTop();
         for (Map.Entry<Integer, Row> entry : rowMap.entrySet()) {
             Row row = entry.getValue();
             for (View v : row.views) {
                 v.layout(left, top, left + v.getMeasuredWidth(), top + v.getMeasuredHeight());
-                Log.i(TAG, "onLayout: " + " left--" + (left) + "top--" + (top) + "--right:" + (left + v.getMeasuredWidth() + horizontal_space) + "--bottom:" + (top + v.getMeasuredHeight()));
+//                Log.i(TAG, "onLayout: " + " left--" + (left + horizontal_space) + "top--" + (top) + "--right:" + (left + v.getMeasuredWidth() + horizontal_space) + "--bottom:" + (top + v.getMeasuredHeight()));
                 left = left + v.getMeasuredWidth() + /*这是孩子左右之间的间距*/horizontal_space;
             }
             top = top + getRowMaxHeight(row) + /*这是孩子上下之间的间距*/vertical_space;
@@ -130,7 +132,7 @@ public class FlowLayout extends ViewGroup {
             }
         }
 
-        return maxHeight + vertical_space;
+        return maxHeight;
     }
 
     private int getTotalHeight(Map<Integer, Row> rowMap) {
@@ -139,28 +141,7 @@ public class FlowLayout extends ViewGroup {
             Row row = entry.getValue();
             sum = sum + getRowMaxHeight(row);
         }
-        return sum;
-    }
-
-    //获取从第一行到第n行的高度
-    private int getRowHeightSum(int rowCount) {
-        int height = 0;
-        if (rowCount == 0) return getPaddingTop();
-        for (int i = 1; i <= rowCount; i++) {
-            Row row = rowMap.get(i);
-            height = height + getRowMaxHeight(row) + vertical_space;
-        }
-        return height;
-    }
-
-
-    private int getRowMaxTopMargin(Row row) {
-        int topMaxMargin = 0;
-        for (View v : row.views) {
-            MarginLayoutParams params = (MarginLayoutParams) v.getLayoutParams();
-            if (params.topMargin > topMaxMargin) topMaxMargin = params.topMargin;
-        }
-        return topMaxMargin;
+        return sum + rowMap.entrySet().size() * vertical_space;
     }
 
     //Row来定义一行来放置多少个View
