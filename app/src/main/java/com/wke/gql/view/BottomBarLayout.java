019d8b2b5b896ddfc92a8214f09d13792d9b7dec
gaoqionglou <438279375@qq.com>
2018-02-24 16:24:34 +0800

    upload

---
+++ app/src/main/java/com/wke/gql/view/BottomBarLayout.java
@@ -0,0 +1,29 @@
+package com.wke.gql.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * @author gql
+ * @since 18-2-24.
+ */
+
+public class BottomBarLayout extends LinearLayout {
+    public BottomBarLayout(Context context) {
+        super(context);
+    }
+
+    public BottomBarLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BottomBarLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BottomBarLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
---
+++ app/src/main/java/com/wke/gql/view/BottomItem.java
@@ -0,0 +1,29 @@
+package com.wke.gql.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * @author gql
+ * @since 18-2-24.
+ */
+
+public class BottomItem extends LinearLayout {
+    public BottomItem(Context context) {
+        super(context);
+    }
+
+    public BottomItem(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BottomItem(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BottomItem(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
