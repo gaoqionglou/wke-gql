80303ab9266c43ed5f5cd09f39f29790a1c6275b
gaoqionglou <gaoqionglou@csair.com>
2018-03-09 17:40:40 +0800

    CityWidget

---
+++ app/src/main/java/com/wke/gql/citywidget/CityDataModel.java
@@ -0,0 +1,16 @@
+package com.wke.gql.citywidget;
+
+import com.wke.gql.greendao.bean.CityItem;
+
+import java.util.List;
+
+/**
+ * @author gql
+ * @since 18-3-9.
+ */
+
+public class CityDataModel {
+    public String code;
+    public String version;
+    private List<CityItem> result;
+}
