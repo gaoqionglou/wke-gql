bc9f193b1a5b0f35cb0c9e52a6582a9317b26582
gaoqionglou <438279375@qq.com>
2021-11-04 15:44:47 +0800

    1

--- app/src/main/java/com/wke/gql/net/retrofit/HintService.java
+++ app/src/main/java/com/wke/gql/net/retrofit/HintService.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 
 public interface HintService {
 //    {"hint":"托运行李最好挂上姓名牌哦~"}
-    @GET("https://3g.csair.com/CSMBP/data/flightstatus/gethint.do?APPTYPE=touch")
+    @GET("")
     public Call<Hint> getHint();
 }
--- app/src/main/java/com/wke/gql/net/retrofit/RxHintService.java
+++ app/src/main/java/com/wke/gql/net/retrofit/RxHintService.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 
 public interface RxHintService {
     //    {"hint":"托运行李最好挂上姓名牌哦~"}
-    @GET("https://3g.csair.com/CSMBP/data/flightstatus/gethint.do?APPTYPE=touch")
+    @GET("")
     public Observable<Hint> getHint();
 }
